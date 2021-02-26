b07f90f26c72324431c774f97e045f5c092716ce
Pawl0 <paulohsantosol@gmail.com>
2021-02-25 23:27:18 -0300

    Added more action types and separated them in constants

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { connect } from 'react-redux';
+import * as actionTypes from '../../store/actions';
 
 const Counter = (props) => {
-
+    const [value, setValue] = useState(0);
     return <div>
         <h1>Current State: {props.ctr}</h1>
+        <input type="number" value={value} onChange={(ev) => setValue(ev.target.value)}/>
         <button onClick={props.onIncrementCounter}>Increment State</button>
+        <button onClick={props.onDecrementCounter}>Decrement State</button>
+        <button onClick={() => props.onAddCounter(value)}>Add State</button>
+        <button onClick={() => props.onSubtractCounter(value)}>Subtract State</button>
+        <hr />
+        <button onClick={props.onStoreResult}>Store Result</button>
+        <ul>
+            {props.results.map((result)=> (
+                <li key={result.id} onClick={() => props.onDeleteResult(result.id)}>{result.value}</li>
+            ))}
+        </ul>
     </div>
 };
 
 const mapStateToProps = (state) => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        results: state.results
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
+        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
+        onAddCounter: (value) => dispatch({type: actionTypes.ADD, value}),
+        onSubtractCounter: (value) => dispatch({type: actionTypes.SUBTRACT, value}),
+        onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, id}),
     };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,42 @@
+import * as actionTypes from './actions';
+
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 };
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
-        case 'INCREMENT':
+        case actionTypes.INCREMENT:
             return {
+                ...state,
                 counter: state.counter+1
+            }
+        case actionTypes.DECREMENT:
+            return {
+                ...state,
+                counter: state.counter-1
+            }
+        case actionTypes.ADD:
+            return {
+                ...state,
+                counter: state.counter+parseInt(action.value, 10)
+            }
+        case actionTypes.SUBTRACT:
+            return {
+                ...state,
+                counter: state.counter-action.value
+            }
+        case actionTypes.STORE_RESULT:
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+            }
+        case actionTypes.DELETE_RESULT:
+            const updatedArray = state.results.filter(result => result.id !== action.id)
+            return {
+                ...state,
+                results: updatedArray
             }
     }
     return state;
